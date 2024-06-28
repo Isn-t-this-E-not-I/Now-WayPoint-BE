31e3382ee712cd0046c553c56d36c06d1c4d2fe3
immyeong <wpfls6670@naver.com>
2024-06-29 00:58:00 +0900

    settings: project setting

---
+++ now-waypoint-core/src/test/java/isn_t_this_e_not_i/now_waypoint_core/NowWaypointCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package isn_t_this_e_not_i.now_waypoint_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowWaypointCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
