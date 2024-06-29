30c5240e2e9b328f85a8e3c97aacec5cfbb3c6a0
immyeong <wpfls6670@naver.com>
2024-06-29 10:38:08 +0900

    test: github action test

--- now-waypoint-core/src/main/java/isn_t_this_e_not_i/now_waypoint_core/NowWaypointCoreApplication.java
+++ now-waypoint-core/src/main/java/isn_t_this_e_not_i/now_waypoint_core/NowWaypointCoreApplication.java
@@ -1,11 +1,9 @@
 package isn_t_this_e_not_i.now_waypoint_core;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
 public class NowWaypointCoreApplication {
 
 	public static void main(String[] args) {
