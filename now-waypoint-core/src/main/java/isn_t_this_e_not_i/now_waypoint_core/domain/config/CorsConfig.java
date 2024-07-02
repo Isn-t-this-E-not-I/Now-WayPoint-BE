b4dbdd5a4703105a0c7ac1ccbdd387009df40a34
immyeong <wpfls6670@naver.com>
2024-07-02 14:02:48 +0900

    ✨ feat: Cors 설정 추가

---
+++ now-waypoint-core/src/main/java/isn_t_this_e_not_i/now_waypoint_core/domain/config/CorsConfig.java
@@ -0,0 +1,15 @@
+package isn_t_this_e_not_i.now_waypoint_core.domain.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
+    }
+}
