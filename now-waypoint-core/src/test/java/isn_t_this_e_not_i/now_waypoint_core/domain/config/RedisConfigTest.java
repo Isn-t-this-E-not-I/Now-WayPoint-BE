287b8d5df9f18024bab6a3c31da88698064b67d4
immyeong <wpfls6670@naver.com>
2024-07-06 00:23:50 +0900

    📌fix: dev에서만 실행되는 테스트 수정

--- now-waypoint-core/src/test/java/isn_t_this_e_not_i/now_waypoint_core/domain/config/RedisConfigTest.java
+++ now-waypoint-core/src/test/java/isn_t_this_e_not_i/now_waypoint_core/domain/config/RedisConfigTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
@@ -13,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class RedisConfigTest {
 
     @Autowired
