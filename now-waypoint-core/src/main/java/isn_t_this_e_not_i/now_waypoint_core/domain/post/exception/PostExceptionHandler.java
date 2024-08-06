e2165c7bb984c6494dadf0d10b00c4acadc63820
JeongHwan Kim <silverice1973@naver.com>
2024-08-06 11:20:13 +0900

    FIX : 게시글 공백일 경우 작성 X

--- now-waypoint-core/src/main/java/isn_t_this_e_not_i/now_waypoint_core/domain/post/exception/PostExceptionHandler.java
+++ now-waypoint-core/src/main/java/isn_t_this_e_not_i/now_waypoint_core/domain/post/exception/PostExceptionHandler.java
@@ -10,12 +10,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class PostExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleUsernameNotFoundException(UsernameNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<String> handleUnauthorizedException(UnauthorizedException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidPostContentException.class)
+    public ResponseEntity<String> handleInvalidPostContentException(InvalidPostContentException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 }
