99269c0877dda73862eab0853bf69091528eff89
immyeong <wpfls6670@naver.com>
2024-08-11 17:56:06 +0900

    build : 배포서버 환경설정 세팅

--- now-waypoint-core/src/main/java/isn_t_this_e_not_i/now_waypoint_core/domain/config/CorsConfig.java
+++ now-waypoint-core/src/main/java/isn_t_this_e_not_i/now_waypoint_core/domain/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedHeaders("Set-Cookie")
                 .allowedHeaders("*")
                 .allowedMethods("*")
-                .allowedOrigins("http://localhost:3000","https://goorm.now-waypoint.store")
+                .allowedOrigins("http://localhost:3000","https://now.waypoint.com")
                 .allowCredentials(true);
     }
 }
