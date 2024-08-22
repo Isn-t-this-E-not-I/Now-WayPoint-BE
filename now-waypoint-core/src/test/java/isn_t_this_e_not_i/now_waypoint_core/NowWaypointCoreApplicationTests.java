42c9f0265ece0af93ad9ec10885ba9b7f9255add
immyeong <wpfls6670@naver.com>
2024-08-22 21:39:48 +0900

    fix: 구글 로그인 설정 오류4

--- now-waypoint-core/src/test/java/isn_t_this_e_not_i/now_waypoint_core/NowWaypointCoreApplicationTests.java
+++ now-waypoint-core/src/test/java/isn_t_this_e_not_i/now_waypoint_core/NowWaypointCoreApplicationTests.java
@@ -1,30 +1,31 @@
 package isn_t_this_e_not_i.now_waypoint_core;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest
 class NowWaypointCoreApplicationTests {
 
-	@DynamicPropertySource
-	static void setup(DynamicPropertyRegistry registry) {
+	@BeforeAll
+	static void setup() {
 		String accessKeyId = System.getenv("CLOUD_AWS_CREDENTIALS_ACCESS_KEY");
 		String secretAccessKey = System.getenv("CLOUD_AWS_CREDENTIALS_SECRET_KEY");
 		String googleClientId = System.getenv("GOOGLE_CLIENT_ID");
 		String googleClientSecret = System.getenv("GOOGLE_CLIENT_SECRET");
 		String fileStoragePath = System.getenv("FILE_STORAGE_PATH");
 
-		if (accessKeyId == null || secretAccessKey == null || fileStoragePath == null || googleClientId == null || googleClientSecret == null) {
-			throw new IllegalArgumentException("Required environment variables are not set");
-		}
+		accessKeyId = accessKeyId != null ? accessKeyId : "defaultAccessKey";
+		secretAccessKey = secretAccessKey != null ? secretAccessKey : "defaultSecretKey";
+		googleClientId = googleClientId != null ? googleClientId : "defaultGoogleClientId";
+		googleClientSecret = googleClientSecret != null ? googleClientSecret : "defaultGoogleClientSecret";
+		fileStoragePath = fileStoragePath != null ? fileStoragePath : "/default/path";
 
-		registry.add("cloud.aws.credentials.access-key", () -> accessKeyId);
-		registry.add("cloud.aws.credentials.secret-key", () -> secretAccessKey);
-		registry.add("file.storage.path", () -> fileStoragePath);
-		registry.add("google.client.id", () -> googleClientId);
-		registry.add("google.client.secret", () -> googleClientSecret);
+		System.setProperty("cloud.aws.credentials.access-key", accessKeyId);
+		System.setProperty("cloud.aws.credentials.secret-key", secretAccessKey);
+		System.setProperty("file.storage.path", fileStoragePath);
+		System.setProperty("google.client.id", googleClientId);
+		System.setProperty("google.client.secret", googleClientSecret);
 	}
 
 	@Test
