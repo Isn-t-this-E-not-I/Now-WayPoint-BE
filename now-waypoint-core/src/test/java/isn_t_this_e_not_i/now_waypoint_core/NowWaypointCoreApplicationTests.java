8a9cb36d895d25e2ebb4cc75a38abae4e3d38293
immyeong <wpfls6670@naver.com>
2024-08-22 21:37:06 +0900

    fix: 구글 로그인 설정 오류3

--- now-waypoint-core/src/test/java/isn_t_this_e_not_i/now_waypoint_core/NowWaypointCoreApplicationTests.java
+++ now-waypoint-core/src/test/java/isn_t_this_e_not_i/now_waypoint_core/NowWaypointCoreApplicationTests.java
@@ -2,13 +2,14 @@ package isn_t_this_e_not_i.now_waypoint_core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest
 class NowWaypointCoreApplicationTests {
 
 	@DynamicPropertySource
-	static void setup() {
+	static void setup(DynamicPropertyRegistry registry) {
 		String accessKeyId = System.getenv("CLOUD_AWS_CREDENTIALS_ACCESS_KEY");
 		String secretAccessKey = System.getenv("CLOUD_AWS_CREDENTIALS_SECRET_KEY");
 		String googleClientId = System.getenv("GOOGLE_CLIENT_ID");
@@ -19,11 +20,11 @@ class NowWaypointCoreApplicationTests {
 			throw new IllegalArgumentException("Required environment variables are not set");
 		}
 
-		System.setProperty("cloud.aws.credentials.access-key", accessKeyId);
-		System.setProperty("cloud.aws.credentials.secret-key", secretAccessKey);
-		System.setProperty("file.storage.path", fileStoragePath);
-		System.setProperty("google.client.id", googleClientId);
-		System.setProperty("google.client.secret", googleClientSecret);
+		registry.add("cloud.aws.credentials.access-key", () -> accessKeyId);
+		registry.add("cloud.aws.credentials.secret-key", () -> secretAccessKey);
+		registry.add("file.storage.path", () -> fileStoragePath);
+		registry.add("google.client.id", () -> googleClientId);
+		registry.add("google.client.secret", () -> googleClientSecret);
 	}
 
 	@Test
